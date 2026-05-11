caf51af75969e41aad5b766363bdecd7edfaadf8
Diwakar Verma <88948065+EpicDivakar@users.noreply.github.com>
2026-05-12 00:23:27 +0530

    Add basic Python examples for data types and sums

---
+++ main.py
@@ -0,0 +1,46 @@
+print("Hello World")
+print("Diwakar Verma")
+print("My name is Diwakar Verma")
+print("and my age is 21")
+
+print("My name is Diwakar and " , "my age is 21 right now")
+
+name = "Diwakar"
+Age = 21
+Profession = 25.9 
+Age2 = Age
+print("My name is : ", name , " and my age is like just" , Age2)
+print(type(name))
+print(type(Age))
+print(type(Profession))
+
+#data types in python 
+
+#1(Intergers)
+
+Age = 2 
+Age2 = -2
+
+#2(String)
+
+name = 'Diwakar'
+name1 = "Diwakar"
+name2 = '''Diwakar'''
+#all these are string and ways to represent string
+
+#3 (Float)
+
+weight = 23.5
+
+#4 (Boolean values)
+
+old = False
+print(old)
+
+
+# A basic sum problem (Problem number 1)
+
+a = 5
+b = 10
+sum = a + b
+print(sum)
